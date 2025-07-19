7e6b05bf68c3404a4ec7c2ce3d000968df09a350
Ja'farbek Yusupov <github.jafarbekyusupov@gmail.com>
2025-07-19 14:06:33 +0500

    fallback for env var OLLAMA_BASE_URL

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
-OLLAMA_BASE_URL = "http://localhost:11434"
+OLLAMA_BASE_URL = os.environ.get('OLLAMA_BASE_URL', "http://localhost:11434")
 
 import logging
 logging.basicConfig(level=logging.INFO)
