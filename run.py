1457e070dc15818dad9aad918d5f8bc657ecfea2
Ja'farbek Yusupov <github.jafarbekyusupov@gmail.com>
2025-07-20 22:55:00 +0500

    add keep alive func against server sleep problm → AWAKE 24/7 + rand ping intervals -- 1-9 min

--- run.py
+++ run.py
@@ -1,11 +1,26 @@
 from flask import Flask
 import threading
+import requests
+import time
 import os
+import random
 from config import TELEGRAM_BOT_TOKEN, GROQ_API_KEY
 from document_bot import DocumentBot
 
+def keep_alive():
+    while True:
+        try:
+            requests.get('https://ai-docs-tgbot.onrender.com/')
+            print("self ping successful")
+        except:
+            print("self ping FAILED")
+        
+        wtime = random.randint(60, 540) # in range btwn 1-9 mins
+        print(f"waiting {wtime//60} mins {wtime%60} secs til next ping")
+        time.sleep(wtime)
+
 def create_health_server():
-    app = Flask(__name__)
+    app = Flask(__name__)    
     @app.route('/')
     def health():
         return '====== | OK | AI DOC BOT is RUNNINNN ======'    
@@ -21,12 +36,19 @@ def main():
     
     bot = DocumentBot(TELEGRAM_BOT_TOKEN, GROQ_API_KEY)
     if os.environ.get('RENDER'):
+        # health server start
         h_app = create_health_server()
         port = int(os.environ.get('PORT', 10000))
         h_thread = threading.Thread(target=lambda: h_app.run(host='0.0.0.0', port=port))
         h_thread.daemon = True
         h_thread.start()
         
+        # kepp alive pinger start
+        ping_thread = threading.Thread(target=keep_alive)
+        ping_thread.daemon = True
+        ping_thread.start()
+        print("health server n keep alive started for render deploy")
+    
     bot.run()
 
 if __name__ == "__main__":
